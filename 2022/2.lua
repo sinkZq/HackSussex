5f999889bd2bb779c6c61ccf4bf202dee42453c1
sinkZq <121246278+sinkZq@users.noreply.github.com>
2024-05-27 22:52:48 +0200

    Update 2.lua

--- 2022/2.lua
+++ 2022/2.lua
@@ -1,1 +1,19 @@
+-- Palindrome string checking (reverse string to check if it's the same as the string un-reversed)
 
+
+function solution(str)
+    local reversedString = string.reverse(str)
+    
+    if str == reversedString then
+        return {true, str, reversedString}
+    end
+
+    return {false, str, reversedString}
+end
+
+local str = "ABBA"
+local checked = solution(str)
+
+for i,v in pairs(checked) do
+    print(i,v)
+end
